9f77d0e5ba881b27aec2a5c4ad7f9401ea3936b8
franonis <2237092085@qq.com>
2021-01-08 21:14:18 +0800

    asd

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -10,32 +10,22 @@ class ResultController extends Controller
     public function getSetPage(Request $request)
     {
         $omics = $request->omics;
-        #设置参数
         $file_data = $request->file_data;
         $file_desc = $request->file_desc;
         $groupsLevel = $request->groupsLevel;
         $data_type = $request->data_type;
-        #输出文件路径
         $outpath = 'uploads/' . $omics . $file_data . $file_desc . md5($file_data . $file_desc) . '/';
         is_dir($outpath . 'results2') or mkdir($outpath . 'results2', 0777, true);
-        #输入文件路径
         $path_datafile = 'uploads/' . $omics . $file_data . md5($file_data) . '/' . $file_data;
         $path_descfile = 'uploads/' . $omics . $file_desc . md5($file_desc) . '/' . $file_desc;
-        #dd($omics);
         if ($omics == "rna") {
-
             $control = $request->control;
             $normalization = $request->normalization;
-            #processing_RNA
-
             if ($data_type == "rna") {
                 $command = 'Rscript /home/zhangqb/program/dev/main_split/processing_RNA.R -a "' . $groupsLevel . '" -i "/home/zhangqb/tttt/public/' . $path_datafile . '" -d "/home/zhangqb/tttt/public/' . $path_descfile . '" -c "' . $control . '" -o "/home/zhangqb/tttt/public/' . $outpath . 'results2/"  -t RNAseq -p "/home/zhangqb/tttt/public/' . $outpath . '"';
             } else {
                 $command = 'Rscript /home/zhangqb/program/dev/main_split/processing_RNA.R -a "' . $groupsLevel . '" -i "/home/zhangqb/tttt/public/' . $path_datafile . '" -d "/home/zhangqb/tttt/public/' . $path_descfile . '" -c "' . $control . '" -o "/home/zhangqb/tttt/public/' . $outpath . 'results2/" -n ' . $normalization . ' -t MiAr -p "/home/zhangqb/tttt/public/' . $outpath . '"';
             }
-            #Rscript  -a "Ly6ChighD4" -i "./branch/benchmark/input/HANgene_tidy_geneid_allgroups.CSV" -d "./branch/benchmark/input/HANsampleList_allgroups.CSV" -c "PMND1" -o "~/temp/results2/"  -t "RNAseq" -p "~/temp/"
-
-            #dd($command);
             if ($this->isRunOver('/home/zhangqb/tttt/public/' . $outpath . 'data.RData')) {
                 $this->showresultrna('/home/zhangqb/tttt/public/' . $outpath);
                 return view('resultrna', ['title' => '上传数据']);
@@ -50,13 +40,10 @@ class ResultController extends Controller
                     return view('resultrna', ['title' => '上传数据']);
                 }
             }
-
         } else {
-            #参数
             $firstline = $request->firstline;
             $delodd = $request->delodd;
             if ($omics == "lipidomics") {
-                #Rscript processing.R -a "all_together" -i "./branch/benchmark/input/HANlipid_tidy.csv" -d "./branch/benchmark/input/HANsampleList_lipid.CSV" -c "" -t "LipidSearch" -f "LipidIon" -l F -o "~/temp/" -n "" -p "~/temp/"
                 $command = 'Rscript /home/zhangqb/program/dev/main_split/processing.R -a "' . $groupsLevel . '" -i "/home/zhangqb/tttt/public/' . $path_datafile . '" -d "/home/zhangqb/tttt/public/' . $path_descfile . '" -t "' . $data_type . '" -c "' . $groupsLevel . '" -f "' . $firstline . '" -l "' . $delodd . '" -o "/home/zhangqb/tttt/public/' . $outpath . 'results2/"  -n "" -p "/home/zhangqb/tttt/public/' . $outpath . '"';
 
                 if ($this->isRunOver('/home/zhangqb/tttt/public/' . $outpath . 'data.RData')) {
@@ -71,9 +58,8 @@ class ResultController extends Controller
                     if ($this->isRunOver('/home/zhangqb/tttt/public/' . $outpath . 'data.RData')) {
                         $this->showresultlip('/home/zhangqb/tttt/public/' . $outpath);
                         return view('resultlip', ['title' => '上传数据']);
+                    }
                 }
-
-                #dd($command);
             } else {
                 $command = 'Rscript /home/zhangqb/program/dev/main_split/processing.R -a "' . $groupsLevel . '" -i "/home/zhangqb/tttt/public/' . $path_datafile . '" -d "/home/zhangqb/tttt/public/' . $path_descfile . '" -t "' . $data_type . '" -c "' . $groupsLevel . '" -f "' . $firstline . '" -l "' . $delodd . '" -o "/home/zhangqb/tttt/public/' . $outpath . 'results2/"  -n "" -p "/home/zhangqb/tttt/public/' . $outpath . '"';
                 if ($this->isRunOver('/home/zhangqb/tttt/public/' . $outpath . 'data.RData')) {
@@ -88,12 +74,10 @@ class ResultController extends Controller
                     if ($this->isRunOver('/home/zhangqb/tttt/public/' . $outpath . 'data.RData')) {
                         $this->showresultmet('/home/zhangqb/tttt/public/' . $outpath);
                         return view('resultmet', ['title' => '上传数据']);
+                    }
                 }
-                #dd($command);
             }
-
         }
-
     }
 
     public function showresultrna($path)
