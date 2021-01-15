5335d6ae880cba39dccd600a3cc3038d6525dd6b
franonis <2237092085@qq.com>
2021-01-15 14:14:04 +0800

    ds

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -190,6 +190,7 @@ class ResultController extends Controller
         }
         $command = '/home/zhangqb/software/ImageMagick/bin/convert -quality 100 -trim ' . $pic_path . 'MARresults/heatmap*.pdf ' . $pic_path . 'MARresults/heatmap_show.png';
         exec($command, $ooout, $flag);
+        dd($ooout);
         if ($flag == 1) {
             return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command]);
         }
