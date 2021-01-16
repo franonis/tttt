7187f395f157df1f1f1296421552bdce41e1eb48
franonis <2237092085@qq.com>
2021-01-16 16:38:07 +0800

    ds

--- resources/views/resultrna.blade.php
+++ resources/views/resultrna.blade.php
@@ -31,7 +31,8 @@
                                 <div class="col-md-6">
                                 </div>
                                 <div class="col-md-6">
-                                    <button href="{{ url('download/png/')}}{{ $pngpath }}results/PCA_show.png" class="layui-btn" >Download</button>
+                                    <i class="layui-icon layui-icon-download-circle" href="{{ url('download/png/')}}{{ $pngpath }}results/PCA_show.png"></i><a>Download</a>
+                                    <a>Download</a>
                                 </div>
                                 <div class="col-md-2">
                                     <h4>PCA result</h4>
