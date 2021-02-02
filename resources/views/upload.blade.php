3893bc56fa9460904c53ccc701a3846450c6c437
franonis <2237092085@qq.com>
2021-02-02 20:24:45 +0800

    ds

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -28,7 +28,7 @@
                         <div class="layui-form-item" id="delodd" style="display: block;">
                           <label class="layui-form-label"></label>
                           <div class="layui-input-block">
-                            <input type="checkbox" checked="" name="delodd" lay-skin="switch" lay-filter="switchTest" lay-text="Yes|No"><a style="margin-top: 8px;">I want delete the odd chain</a style="margin-top: 8px;">
+                            <input type="checkbox" checked="" name="delodd" lay-skin="switch" lay-filter="switchTest" lay-text="Yes|No"><label style="margin-top: 8px;">I want delete the odd chain</label>
                           </div>
                         </div>
                         <p style="margin-left: 0px;">If you don`t know what to upload, you can click our example to download the file.</p><br>
