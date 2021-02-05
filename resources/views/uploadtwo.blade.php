6320325bf6089c90ac3165ddcfa39dcb9f36c912
franonis <2237092085@qq.com>
2021-02-05 16:22:38 +0800

    ds

--- resources/views/uploadtwo.blade.php
+++ resources/views/uploadtwo.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('css')
-<link rel="stylesheet" type="text/css" href="{{ asset('/layui/dist/css/layui.css') }}"  media="all">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/layui/dist/css/layui.css') }}"  media="all">
 
 @endsection
 @section('navbar')
@@ -10,10 +10,11 @@
 <div class="container content">
     <div class="row">
         @include('partials.errors')
-        <h3>Upload your data</h3>
+        <p><a style="font-size: 200%;">Upload your data</a> / Set Parameters / Show the statistical results</p>
         <hr>
         <div class="col-md-12">
-            <form class="layui-form" action="/crosscanshu">
+            <div class="col-md-12">
+                <form class="layui-form" action="/crosscanshu">
                 <div class="col-md-6">
                     <div class="col-md-12">
                         <div style="padding: 20px; background-color: #F2F2F2;">
@@ -87,13 +88,14 @@
                 </div>
                 <br>
             </form>
+            </div>
         </div>
+
         <br>
         <h3>Try example data</h3>
         <hr>
-        <form class="layui-form" action="/crosscanshu">
-            <div class="col-md-6">
-                <table class="layui-table">
+        <form class="layui-form" action="/examplecanshu" id="example">
+            <table class="layui-table">
                 <colgroup>
                   <col width="33%">
                   <col width="33%">
@@ -101,25 +103,27 @@
                 </colgroup>
                 <thead>
                   <tr>
-                    <th>Example</th>
-                    <th>Omics1 file</th>
-                    <th>Omics2 file</th>
+                    <th>Data Type</th>
+                    <th>Data file</th>
                     <th>Descriptional file</th>
                   </tr>
                 </thead>
                 <tbody id="exampleomics">
                   <tr>
-                    <td><input type="radio" name="exampleomics" value="Example1" title="Example1" checked=""></td><td><a href="{{ url('download/example').'/' }}HANLipidMediator_imm_forcor.CSV " >HANLipidMediator_imm_forcor.CSV </a></td><td><a href="{{ url('download/example').'/' }}HANgene_tidy.CSV" >HANgene_tidy.CSV</a></td><td><a href="{{ url('download/example').'/' }}HANsampleList_lipmid.csv" >HANsampleList_lipmid.csv</a><br><a href="{{ url('download/example').'/' }}HANsampleList.CSV" >HANsampleList.CSV</a></td>
+                    <td><input type="radio" name="exampleomics" value="Lipidomics" title="Lipidomics" checked=""></td><td><a href="{{ url('download/example').'/' }}HANlipid_tidy.csv" >HANlipid_tidy.csv</a></td><td><a href="{{ url('download/example').'/' }}HANsampleList_lipid.CSV" >HANsampleList_lipid.CSV</a></td>
                   </tr><tr>
-                    <td><input type="radio" name="exampleomics" value="Example2" title="Example2"></td><td><a href="{{ url('download/example').'/' }}lipids.csv" >lipids.csv</a></td><td><a href="{{ url('download/example').'/' }}RNAseq_genesymbol.csv" >RNAseq_genesymbol.csv</a></td><td><a href="{{ url('download/example').'/' }}sampleList.csv" >sampleList.csv</a></td>
+                    <td><input type="radio" name="exampleomics" value="Lipidomicscos" title="Lipidomics"></td><td><a href="{{ url('download/example').'/' }}Cos7_integ_2.csv" >Cos7_integ_2.csv</a></td><td><a href="{{ url('download/example').'/' }}Cos7_integ_sampleList.csv" >Cos7_integ_sampleList.csv</a></td>
                   </tr><tr>
-                    <td><input type="radio" name="exampleomics" value="Example3" title="Example3"></td><td><a href="{{ url('download/example').'/' }}metabolites.csv" >metabolites.csv</a></td><td><a href="{{ url('download/example').'/' }}RNAseq_genesymbol.csv" >RNAseq_genesymbol.csv</a></td><td><a href="{{ url('download/example').'/' }}sampleList.csv" >sampleList.csv</a></td>
+                    <td><input type="radio" name="exampleomics" value="Metabolomics" title="Metabolomics"></td><td><a href="{{ url('download/example').'/' }}metabolites_tidy2.csv" >metabolites_tidy2.csv</a></td><td><a href="{{ url('download/example').'/' }}sampleList_lip.csv" >sampleList_lip.csv</a></td>
                   </tr><tr>
-                    <td><input type="radio" name="exampleomics" value="Example3" title="Example3"></td><td><a href="{{ url('download/example').'/' }}metabolites_tidy2.csv" >metabolites_tidy2.csv</a></td><td><a href="{{ url('download/example').'/' }}proteins_Depletion_tidy.csv " >proteins_Depletion_tidy.csv </a></td><td><a href="{{ url('download/example').'/' }}sampleList_lip.csv" >sampleList_lip.csv</a></td>
+                    <td><input type="radio" name="exampleomics" value="Transcriptomics" title="Transcriptomics"></td><td><a href="{{ url('download/example').'/' }}gene_tidy.CSV" >gene_tidy.CSV</a></td><td><a href="{{ url('download/example').'/' }}sampleList.CSV" >sampleList.CSV</a></td>
+                  </tr><tr>
+                    <td><input type="radio" name="exampleomics" value="Transcriptomicshan" title="Transcriptomics"></td><td><a href="{{ url('download/example').'/' }}HANgene_tidy_geneid_allgroups.CSV" >HANgene_tidy_geneid_allgroups.CSV</a></td><td><a href="{{ url('download/example').'/' }}HANsampleList_allgroups.CSV" >HANsampleList_allgroups.CSV</a></td>
+                  </tr><tr>
+                    <td><input type="radio" name="exampleomics" value="Proteomics" title="Proteomics"></td><td><a href="{{ url('download/example').'/' }}proteins_Depletion_tidy.csv" >proteins_Depletion_tidy.csv</a></td><td><a href="{{ url('download/example').'/' }}sampleList_lip.csv" >sampleList_lip.csv</a></td>
                   </tr>
                 </tbody>
             </table>
-            </div>
             <div class="col-md-12 text-center">
                 <br>
                 <button id="submitexample" class="layui-btn" type="submit">Continue</button>
@@ -130,39 +134,10 @@
 @endsection
 @section('footer')
   @include('partials.footer')
+
 @endsection
 @section('js')
-<script href="{{ asset('/layui/layui-2.4.5/dist/layui.all.js') }}" ></script>
-<script href="{{ asset('/layer/layer.js') }}"></script>
-<script>
-    $(document).ready(function(){
-        changetheprogram();
-        layui.use('form', function(){
-          var form = layui.form; //只有执行了这一步，部分表单元素才会自动修饰成功
-        });
-
-    });
-
-    $("#omics_left").click(function (){
-        name =$("input[name='omics_left']:checked").val();
-        if (name == "Lipidomics") {
-          document.getElementById("delodd").style.display="block";
-        }
-        if (name == "Metabolomics") {
-          document.getElementById("delodd").style.display="none";
-        }
-    });
-    $("#omics_right").click(function (){
-        name =$("input[name='omics_right']:checked").val();
-        if (name == "Proteinomics") {
-          document.getElementById("dataType").style.display="none";
-        }
-        if (name == "Transcriptomics") {
-          document.getElementById("dataType").style.display="block";
-        }
-        console.log(name);
-    });
-</script>
+<script src="{{ asset('/layui/dist/layui.js') }}" charset="utf-8"></script>
 <script>
 
 layui.use('upload', function(){
@@ -170,7 +145,7 @@ layui.use('upload', function(){
 
   //执行实例
   var uploadInst = upload.render({
-    elem: '#right1'
+    elem: '#datafile'
     ,accept:'file'
     ,method: 'POST'
     ,data:{
@@ -179,12 +154,12 @@ layui.use('upload', function(){
     ,url: '/uploadfile' //改成您自己的上传接口
     ,done: function(res){
       layer.msg('upload succeed');
-      $("#file_right1").val(res.originalname);
+      $("#file_datafile").val(res.originalname);
       console.log(res)
     }
   });
   var uploadInst = upload.render({
-    elem: '#right2'
+    elem: '#descfile'
     ,accept:'file'
     ,method: 'POST'
     ,data:{
@@ -193,39 +168,37 @@ layui.use('upload', function(){
     ,url: '/uploadfile' //改成您自己的上传接口
     ,done: function(res){
       layer.msg('upload succeed');
-      $("#file_right2").val(res.originalname);
-      console.log(res)
-    }
-  });
-  var uploadInst = upload.render({
-    elem: '#left1'
-    ,accept:'file'
-    ,method: 'POST'
-    ,data:{
-        '_token':'{{csrf_token()}}'
-    }
-    ,url: '/uploadfile' //改成您自己的上传接口
-    ,done: function(res){
-      layer.msg('upload succeed');
-      $("#file_left1").val(res.originalname);
-      console.log(res)
-    }
-  });
-  var uploadInst = upload.render({
-    elem: '#left2'
-    ,accept:'file'
-    ,method: 'POST'
-    ,data:{
-        '_token':'{{csrf_token()}}'
-    }
-    ,url: '/uploadfile' //改成您自己的上传接口
-    ,done: function(res){
-      layer.msg('upload succeed');
-      $("#file_left2").val(res.originalname);
+      $("#file_descfile").val(res.originalname);
       console.log(res)
     }
   });
 });
 </script>
+<script>
+    $(document).ready(function(){
+        layui.use('form', function(){
+          var form = layui.form; //只有执行了这一步，部分表单元素才会自动修饰成功
+        });
 
+        layui.use('table', function(){
+          var table = layui.table; //只有执行了这一步，部分表单元素才会自动修饰成功
+        });
+    });
+    $("#omics").click(function (){
+        name =$("input[name='omics']:checked").val();
+        if (name == "Lipidomics") {
+          document.getElementById("delodd").style.display="block";
+          document.getElementById("dataType").style.display="none";
+        }
+        if (name == "Metabolomics" || name == "Proteomics") {
+          document.getElementById("delodd").style.display="none";
+          document.getElementById("dataType").style.display="none";
+        }
+        if (name == "Transcriptomics") {
+          document.getElementById("dataType").style.display="block";
+          document.getElementById("delodd").style.display="none";
+        }
+        console.log(name);
+   });
+</script>
 @endsection
