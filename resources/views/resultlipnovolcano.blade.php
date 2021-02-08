9a53f82f28a7f25b44b46551272587463b282e64
franonis <2237092085@qq.com>
2021-02-08 22:35:20 +0800

    ds

--- resources/views/resultlipnovolcano.blade.php
+++ resources/views/resultlipnovolcano.blade.php
@@ -146,15 +146,6 @@
                                             <div class="layui-colla-content layui-show">
                                                 <a href="{{ url('download/file/')}}/{{ $downloadpath }}headgroup++headgroupcolor_show.png"><i class="layui-icon layui-icon-download-circle" style="font-size: 30px;"></i>Download this picture</a>
                                                 <img src="http://www.lintwebomics.info/{{ $path }}results/headgroup/headgroupcolor_show.png" style="height:50%;width: 60%;">
-                                                <div class="layui-carousel" id="test1" lay-filter="test1">
-                                                  <div carousel-item="">
-                                                    @foreach($headpng as $k=>$i )
-                                                        <div>
-                                                            <img src="http://www.lintwebomics.info/{{ $path }}results/headgroup/others_{{$i}}.png" data-holder-rendered="true">
-                                                        </div>
-                                                    @endforeach
-                                                  </div>
-                                                </div>
                                             </div>
                                           </div>
                                           <div class="layui-colla-item">
@@ -213,15 +204,6 @@
                                         <h4>Lipid Fatty acid Statisics</h4>
                                     </div>
                                     <div class="col-md-10">
-                                        <div class="layui-carousel" id="test2" lay-filter="test2">
-                                          <div carousel-item="">
-                                            @foreach($fapng as $k=>$i )
-                                                <div>
-                                                    <img src="http://www.lintwebomics.info/{{ $path }}results/FAchainVisual/others_{{$i}}.png" data-holder-rendered="true">
-                                                </div>
-                                            @endforeach
-                                          </div>
-                                        </div>
                                     </div>
                             </div>
                     </div>
