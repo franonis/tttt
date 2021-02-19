9e5dd15b3f630cd31736cea2cc8e35caca11cb70
franonis <2237092085@qq.com>
2021-02-20 07:57:59 +0800

    ds

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,28 +16,6 @@
 </head>
 
   <div class="site-content">
-    <div style="padding: 20px; background-color: #F2F2F2;">
-                      <div class="layui-row layui-col-space15">
-                        <div class="layui-col-md6">
-                          <div class="layui-card">
-                            <div class="layui-card-header">single omics analysis</div>
-                            <div class="layui-card-body">
-                              something introduce single omics analysis<br>
-                              <a href="/single" style="color: deepskyblue;font-size: 180%;">>>>click here to start<<<</a>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="layui-col-md6">
-                          <div class="layui-card">
-                            <div class="layui-card-header">intro omics analysis</div>
-                            <div class="layui-card-body">
-                              something introduce single omics analysis<br>
-                              <a href="/intro" style="color: deepskyblue;font-size: 180%;">>>>click here to start<<<</a>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div> 
 
         <section class="headintro">
             <div class="splitback">
@@ -104,7 +82,18 @@
   @include('partials.footer')
 @endsection
 @section('js')
-
+<script src="{{ asset('/layui/dist/layui.js') }}" charset="utf-8"></script>
+<script>
+layui.use(['element', 'layer'], function(){
+  var element = layui.element;
+  var layer = layui.layer;
+  
+  //监听折叠
+  element.on('collapse(test)', function(data){
+    layer.msg('展开状态：'+ data.show);
+  });
+});
+</script>
 
 
 
