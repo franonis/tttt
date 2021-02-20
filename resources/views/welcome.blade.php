acb7c34d4d2c13b894068d51b2d068374c63a535
franonis <2237092085@qq.com>
2021-02-20 08:04:08 +0800

    ds

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,28 @@
 </head>
 
   <div class="site-content">
+        <div style="padding: 20px; background-color: #F2F2F2;">
+                      <div class="layui-row layui-col-space15">
+                        <div class="layui-col-md6">
+                          <div class="layui-card">
+                            <div class="layui-card-header">single omics analysis</div>
+                            <div class="layui-card-body">
+                              something introduce single omics analysis<br>
+                              <a href="/single" style="color: deepskyblue;font-size: 180%;">>>>click here to start<<<</a>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="layui-col-md6">
+                          <div class="layui-card">
+                            <div class="layui-card-header">intro omics analysis</div>
+                            <div class="layui-card-body">
+                              something introduce single omics analysis<br>
+                              <a href="/intro" style="color: deepskyblue;font-size: 180%;">>>>click here to start<<<</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
 
         <section class="headintro">
             <div class="splitback">
