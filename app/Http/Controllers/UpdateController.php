b84ca94e1680b42337002f2b2a13419bac9e59a5
franonis <2237092085@qq.com>
2021-04-15 16:19:26 +0800

    ds

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -22,7 +22,7 @@ class UpdateController extends Controller
         $r_path = '/home/zhangqb/tttt/public/' . $path . '../';
         $pic_path = '/home/zhangqb/tttt/public/' . $path;
 
-        #exec('rm '.$pic_path.'MARresults/*');
+        exec('rm '.$pic_path.'MARresults/volcano_reg_*.pdf');
         
         $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/main_split/lipVolcanoPlot.R -r "' . $r_path . '" -s ' . $s . ' -p "' . $pic_path . '" -b F -x "' . $x . '" -j ' . $j . ' -k ' . $k . ' -m ' . $m . ' -w ' . $w . ' ';
         #dd($command);
@@ -52,7 +52,7 @@ class UpdateController extends Controller
         $r_path = '/home/zhangqb/tttt/public/' . $path . '../';
         $pic_path = '/home/zhangqb/tttt/public/' . $path;
 
-        #exec('rm '.$pic_path.'MARresults/*');
+        exec('rm '.$pic_path.'MARresults/heatmap_top*.pdf');
 
 
         $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/main_split/lipHeatmapPlot.R -r "' . $r_path . '" -y "' . $pic_path . '" -e ' . $e;
@@ -80,7 +80,7 @@ class UpdateController extends Controller
         $r_path = '/home/zhangqb/tttt/public/' . $path . '../';
         $pic_path = '/home/zhangqb/tttt/public/' . $path;
 
-        #exec('rm '.$pic_path.'headgroup/*');
+        exec('rm '.$pic_path.'headgroup/heatmap_lipClassSummary_*.pdf');
 
         $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/main_split/lipSumClassHeatmapPlot.R -r "' . $r_path . '" -u "' . $pic_path . '" -w '.$w.' -z ' . $z;
         #dd($command);
@@ -104,7 +104,7 @@ class UpdateController extends Controller
         $r_path = '/home/zhangqb/tttt/public/' . $path . '../';
         $pic_path = '/home/zhangqb/tttt/public/' . $path;
 
-        #exec('rm '.$pic_path.'headgroup/*');
+        exec('rm '.$pic_path.'headgroup/headgroup*pdf');
 
         $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/main_split/headgroupStat.R -r "' . $r_path . '" -u "' . $pic_path . '" -w T';
         #dd($command);
@@ -153,7 +153,7 @@ class UpdateController extends Controller
         $r_path = '/home/zhangqb/tttt/public/' . $path . '../';
         $pic_path = '/home/zhangqb/tttt/public/' . $path;
 
-        #exec('rm '.$pic_path.'FAchainVisual/*');
+        exec('rm '.$pic_path.'FAchainVisual/*pdf');
 
         $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/main_split/FAchainStat.R -r "' . $r_path . '" -v "' . $pic_path . '" -g "'.$g.'" -w '.$w.' -e '.$e;
 
@@ -262,7 +262,7 @@ class UpdateController extends Controller
         #dd($r_path);
         $enrich_path = '/home/zhangqb/tttt/public/' . $path.'enrich/';
 
-        #exec('rm '.$enrich_path.'*');
+        exec('rm '.$enrich_path.'*pdf');
         $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/enrich/met_preEnrich.R -r "' . $r_path . '"  -j '.$j.' -k '.$k.' -p "' . $enrich_path . '"';
         #dd($command);
         exec($command, $ooout, $flag);
@@ -304,7 +304,7 @@ class UpdateController extends Controller
         $r_path = '/home/zhangqb/tttt/public/' . $path . '../';
         $pic_path = '/home/zhangqb/tttt/public/' . $path;
 
-        #exec('rm '.$pic_path.'*');
+        exec('rm '.$pic_path.'volcano_*.pdf');
 
         #火山图Rscript rnaVolcanoPlot.R -r "~/temp/" -s "~/temp/results2/" -f 2.0 -p 0.1 -u 20
         $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/main_split/rnaVolcanoPlot.R -r "' . $r_path . '" -s "' . $pic_path . '" -f ' . $f . ' -p ' . $p . ' -u ' . $u;
@@ -402,7 +402,7 @@ class UpdateController extends Controller
             return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command]);
         }
         exec('cp data_circos.RData '.'/home/zhangqb/tttt/public/'.$opath.'enrich/');
-        $command = '/home/zhangqb/software/ImageMagick/bin/convert -quality 100 -trim /home/zhangqb/tttt/public/'.$opath.'enrich/GOenrich_Biological_Process.pdf /home/zhangqb/tttt/public/'.$opath.'enrich/GOenrich.png';
+        $command = '/home/zhangqb/software/ImageMagick/bin/convert -quality 100 -trim /home/zhangqb/tttt/public/'.$opath.'enrich/GOenrich_'.$c.'.pdf /home/zhangqb/tttt/public/'.$opath.'enrich/GOenrich.png';
         exec($command, $ooout, $flag);
         return response()->json(['code'=> 'success']);
     }
@@ -421,88 +421,11 @@ class UpdateController extends Controller
         $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/correlation/circos_plot.R -r "/home/zhangqb/tttt/public/'.$opath.'enrich/" -i "/home/zhangqb/tttt/public/'.$opath.'" -j '.$j.' -k '.$k.' -t '.$t.' -n '.$n.' -o "/home/zhangqb/tttt/public/'.$opath.'enrich/"';
         exec($command, $ooout, $flag);
         if ($flag == 1) {
-            return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command]);
+            #return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command]);
         }
         $command = '/home/zhangqb/software/ImageMagick/bin/convert -quality 100 -trim /home/zhangqb/tttt/public/'.$opath.'enrich/circosPlot.pdf /home/zhangqb/tttt/public/'.$opath.'enrich/circosPlot.png';
         exec($command, $ooout, $flag);
         return response()->json(['code'=> 'success']);
-    }
-
-    public function showresultrna2($data)
-    {
-        $r_path = '/home/zhangqb/tttt/public/' . $path . '../';
-        $pic_path = '/home/zhangqb/tttt/public/' . $path;
- #$path是上一个处理数据程序的输出目录 $pic_path是本程序的输出目录
-        is_dir($pic_path) or mkdir($pic_path, 0777, true);
-
-        $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/main_split/show_variability.R -r "' . $r_path . '" -o "' . $pic_path . '"';
-        #dd($command);
-        exec($command, $ooout, $flag);
-        if ($flag == 1) {
-            return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command]);
-        }
-        $command = '/home/zhangqb/software/ImageMagick/bin/convert -quality 100 -trim ' . $pic_path . 'PCA_score_plot_*.pdf ' . $pic_path . 'PCA_show.png';
-        exec($command, $ooout, $flag);
-        if ($flag == 1) {
-            return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command]);
-        }
-
-        #火山图Rscript rnaVolcanoPlot.R -r "~/temp/" -s "~/temp/results2/" -f 2.0 -p 0.1 -u 20
-        $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/main_split/rnaVolcanoPlot.R -r "' . $r_path . '" -s "' . $pic_path . '" -f 2.0 -p 0.1 -u 20';
-        exec($command, $ooout, $flag);
-        if ($flag == 1) {
-            return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command]);
-        }
-        $command = '/home/zhangqb/software/ImageMagick/bin/convert -quality 100 -trim ' . $pic_path . 'volcano*.pdf ' . $pic_path . 'heatmap_show.png';
-        exec($command, $ooout, $flag);
-        if ($flag == 1) {
-            return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command]);
-        }
-        #热图Rscript rnaHeatmapPlot.R -r "~/temp/" -w "~/temp/results2/" -v 75
-        $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/main_split/rnaHeatmapPlot.R -r "' . $r_path . '" -w "' . $pic_path . '" -v 75';
-        exec($command, $ooout, $flag);
-        if ($flag == 1) {
-            return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command]);
-        }
-        $command = '/home/zhangqb/software/ImageMagick/bin/convert -quality 100 -trim ' . $pic_path . 'heatmap_allgroups.pdf ' . $pic_path . 'heatmap_allgroups.png';
-        exec($command, $ooout, $flag);
-        if ($flag == 1) {
-            return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command]);
-        }
-        $command = '/home/zhangqb/software/ImageMagick/bin/convert -quality 100 -trim ' . $pic_path . 'heatmap_top*.pdf ' . $pic_path . 'heatmap_show.png';
-        exec($command, $ooout, $flag);
-        #dd($ooout);
-        if ($flag == 1) {
-            return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command]);
-        }
-        #dd($path . 'results/');
-        return 1;
-
-    }
-
-    public function getdownloadfilename($downloadpath)
-    {
-        #volcano
-        $command='cd '.$downloadpath.'MARresults/ && ls volcano*';
-        exec($command,$volcano,$flag);
-        $download["volcano"]=$volcano;
-        #heatmap
-        $command='cd '.$downloadpath.'MARresults/ && ls heatmap*';
-        exec($command,$heatmap,$flag);
-        $download["heatmap"]=$heatmap;
-        #headgroup
-        $command='cd '.$downloadpath.'headgroup/ && ls';
-        exec($command,$headgroup,$flag);
-        $download["headgroup"]=$headgroup;
-        #FAchainVisual
-        $command='cd '.$downloadpath.'FAchainVisual/ && ls';
-        exec($command,$FAchainVisual,$flag);
-        $download["FAchainVisual"]=$FAchainVisual;
-        #enrich
-        $command='cd '.$downloadpath.'enrich/ && ls';
-        exec($command,$enrich,$flag);
-        $download["enrich"]=$enrich;
-        return $download;
     }
 
     private function isRunOver($file)
