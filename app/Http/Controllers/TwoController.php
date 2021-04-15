de8a5eacfce2558ccf3a887938ced190a9c599b0
franonis <2237092085@qq.com>
2021-04-15 08:35:53 +0800

    ds

--- app/Http/Controllers/TwoController.php
+++ app/Http/Controllers/TwoController.php
@@ -166,7 +166,8 @@ class TwoController extends Controller
         $hang=explode(",", $splits[2][0]);#宽
         $lie=explode(",", $splits[3][0]);#高
         #dd($hang);
-        $baijiange = -3;
+        $shujiange = -3;
+        $hengjiange = 3;
         
 
 
@@ -179,15 +180,18 @@ class TwoController extends Controller
         $k2 = $k;
         $fgwidth = floor($size[0] / $g);
         $fgheigh = floor($size[1] / $k2);
+        $diyihangkongbai = '<img style="width: {{ $bgwidth }}px;height: {{ $kongbai[1] }}px;opacity: 88%; margin-top: -10px;" src="{{ asset("images/gg.png") }}" />'
         if ($b == "k_means") {
             #$kongbai[0]=$kongbai[0]*0.2;
-            $baijiange = 0;
-            $kongbai2=$size[0] - array_sum($hang) - $kongbai[0]-count($hang)*8;
+            $shujiange = 0;
+            $kongbai2=$size[0] - array_sum($hang) - $kongbai[0]-count($hang)*4;
+            $hengjiange = 7;
+            $diyihangkongbai = <p style="display: none;"></p>
         }
 
         #dd($omics1);
         $enrichpath = preg_replace('/\//', "++", $outpath);#下载的时候用
-        return view('crossresult', ['image' => $image, 'enrichpath' => $enrichpath, 'bgwidth' => $bgwidth, 'bgheigh' => $bgheigh, 'fgwidth' => $fgwidth, 'fgheigh' => $fgheigh, 'g' => $g, 'k2' => $k2, 'kongbai' => $kongbai, 'kongbai2' => $kongbai2, 'baijiange' => $baijiange,'hang' => $hang, 'lie' => $lie, 'omics1' => $omics1, 'omics2' => $omics2]);
+        return view('crossresult', ['image' => $image, 'enrichpath' => $enrichpath, 'bgwidth' => $bgwidth, 'bgheigh' => $bgheigh, 'fgwidth' => $fgwidth, 'fgheigh' => $fgheigh, 'g' => $g, 'k2' => $k2, 'kongbai' => $kongbai, 'kongbai2' => $kongbai2, 'shujiange' => $shujiange, 'hengjiange' => $hengjiange,'hang' => $hang, 'lie' => $lie, 'omics1' => $omics1, 'omics2' => $omics2, 'diyihangkongbai' => $diyihangkongbai]);
     }
 
     public function getenrichPage($pos)
--- resources/views/crossresult.blade.php
+++ resources/views/crossresult.blade.php
@@ -20,13 +20,13 @@
                   <div class="layui-card-header">{{$omics1}} and {{$omics2}} Correlation analysis result, click the brick for more details</div>
                   <div class="layui-card-body">
                     <div class="row text-center" style="width: {{ $bgwidth }}px;height: {{ $bgheigh }}px; background-image: url({{ asset($image) }}); ">
-                        <img style="width: {{ $bgwidth }}px;height: {{ $kongbai[1] }}px;opacity: 88%; margin-top: -10px;" src="{{ asset('images/gg.png') }}" />
+                        {!! $diyihangkongbai !!}
                         @for ($i = 0; $i < $k2; $i++)
-                            <img style="width: {{ $kongbai[0] }}px;height: {{ $lie[$i] }}px;opacity: 88%; margin-bottom: 3px;margin-left: -3px;" src="{{ asset('images/gg.png') }}" />
+                            <img style="width: {{ $kongbai[0] }}px;height: {{ $lie[$i] }}px;opacity: 88%; margin-bottom: {{ $hengjiange }}px;margin-left: -3px;" src="{{ asset('images/gg.png') }}" />
                             @for ($j = 0; $j < $g; $j++)
-                                <a href="{{ url('result/enrich/')}}/{{$j}}--{{$i}}--{{$enrichpath}}--{{$omics1}}--{{$omics2}}" target="_blank"><img style="width: {{ $hang[$j] }}px;height: {{ $lie[$i] }}px;opacity: 50%; margin-bottom: 3px;margin-left: {{ $baijiange }}px;" src="{{ asset('images/gg.png') }}" onmousemove="toumingImg(this)" onmouseout="normalImg(this)" /></a>
+                                <a href="{{ url('result/enrich/')}}/{{$j}}--{{$i}}--{{$enrichpath}}--{{$omics1}}--{{$omics2}}" target="_blank"><img style="width: {{ $hang[$j] }}px;height: {{ $lie[$i] }}px;opacity: 50%; margin-bottom: {{ $hengjiange }}px;margin-left: {{ $shujiange }}px;" src="{{ asset('images/gg.png') }}" onmousemove="toumingImg(this)" onmouseout="normalImg(this)" /></a>
                             @endfor
-                            <img style="width: {{ $kongbai2 }}px;height: {{ $lie[$i] }}px;opacity: 88%; margin-bottom: 3px;" src="{{ asset('images/gg.png') }}" />
+                            <img style="width: {{ $kongbai2 }}px;height: {{ $lie[$i] }}px;opacity: 88%; margin-bottom: {{ $hengjiange }}px;" src="{{ asset('images/gg.png') }}" />
                         @endfor
                     </div>
                   </div>
