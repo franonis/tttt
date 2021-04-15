fe3e7978588d8f1b14b02e29125089198be486be
franonis <2237092085@qq.com>
2021-04-15 17:53:11 +0800

    ds

--- app/Http/Controllers/TwoController.php
+++ app/Http/Controllers/TwoController.php
@@ -214,7 +214,8 @@ class TwoController extends Controller
             if (!$this->isRunOver('/home/zhangqb/tttt/public/' .$opath.'enrich/ora_dpi72.png') ){
                 exec($command, $ooout, $flag);
                 if ($flag == 1) {
-                    return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command.$ooout]);
+                    dd($ooout);
+                    return view('errors.200', ['title' => 'RUN ERROR', 'msg' => $command.$flag]);
                 }
             }
             if ($this->isRunOver('/home/zhangqb/tttt/public/' .$opath.'enrich/ora_dpi72.png') ){
