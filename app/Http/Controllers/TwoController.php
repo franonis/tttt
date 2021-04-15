e72d3057402e628bb75c863d030ef219af411a37
franonis <2237092085@qq.com>
2021-04-15 09:21:24 +0800

    ds

--- app/Http/Controllers/TwoController.php
+++ app/Http/Controllers/TwoController.php
@@ -182,11 +182,11 @@ class TwoController extends Controller
         $fgheigh = floor($size[1] / $k2);
         $diyihangkongbai = '<img style="width:'. $bgwidth.'px;height:'. $kongbai[1].'px;opacity: 88%; margin-top: -10px;" src="http://www.lintwebomics.info/images/gg.png" />';
         if ($b == "k_means") {
-            #$kongbai[0]=$kongbai[0]*0.2;
+            $kongbai[0]=$kongbai[0]*0.95;
             $shujiange = -2;
             $kongbai2=$size[0] - array_sum($hang) - $kongbai[0]-count($hang)*4;
             $hengjiange = 4;
-            $diyihangkongbai = '<div style="display: none;width:'. $bgwidth.'px;height:'. $kongbai[1].'px;opacity: 88%; margin-top: -10px;" ><p>.</p></div>';
+            $diyihangkongbai = '<div style="display: none;width:'. $bgwidth.'px;height:'. $kongbai[1].'px;opacity: 88%;" ><p>.</p></div>';
         }
 
         #dd($omics1);
