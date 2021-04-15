58dc0aefa0a396bb1dafecab25af00b6625a1237
franonis <2237092085@qq.com>
2021-04-15 14:56:47 +0800

    ds

--- resources/views/resultlipnovolcano.blade.php
+++ resources/views/resultlipnovolcano.blade.php
@@ -612,7 +612,6 @@
             success: function (data) {
                 if(data.code == 'success'){
                     console.log("keyi");
-                    document.getElementById("fashow").src = data.show+'?t='+'+Math.random()';
                     document.getElementById("faheatmappng").src = data.heatmap+'?t='+'+Math.random()';
                     console.log(data.pngnum);
                     for (var i = 0; i < data.pngnum; i++) {
