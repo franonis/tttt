47353d17bcee88a8c1c09d9f618d05539cc6a2e0
franonis <2237092085@qq.com>
2021-04-19 10:19:33 +0800

    ds

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -388,7 +388,7 @@ class UpdateController extends Controller
             $g = $datas[6];
         }
         
-        $opath = preg_replace('/\+\+/', "/", $downloadpath);#$opath = preg_replace('/\//', "++", $outpath);#末尾有/
+        $opath = $path.'enrich/';#$opath = preg_replace('/\//', "++", $outpath);#末尾有/
         #exec('rm '.$opath.'enrich/GOenrich*');
         if ($omics2 == "Transcriptomics") {
             $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/enrich/geneCorEnrich.R -i "/home/zhangqb/tttt/public/'.$opath.'" -k '.$k.' -t "'.$t.'" -g "'.$g.'" -s '.$s.' -c "'.$c.'" -o "/home/zhangqb/tttt/public/'.$opath.'enrich/"';
@@ -416,7 +416,7 @@ class UpdateController extends Controller
         $t = $datas[3];
         $n = $datas[4];
 
-        $opath = preg_replace('/\+\+/', "/", $downloadpath);#$opath = preg_replace('/\//', "++", $outpath);#末尾有/
+        $opath = $path.'enrich/';#$opath = preg_replace('/\//', "++", $outpath);#末尾有/
 
         $command = '/home/new/R-3.6.3/bin/Rscript /home/zhangqb/tttt/public/program/dev/correlation/circos_plot.R -r "/home/zhangqb/tttt/public/'.$opath.'enrich/" -i "/home/zhangqb/tttt/public/'.$opath.'" -j '.$j.' -k '.$k.' -t '.$t.' -n '.$n.' -o "/home/zhangqb/tttt/public/'.$opath.'enrich/"';
         exec($command, $ooout, $flag);
