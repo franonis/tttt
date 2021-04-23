ce50207c88cfd626a37286f0dff917cd60977610
franonis <2237092085@qq.com>
2021-04-23 16:19:45 +0800

    ds

--- app/Http/Controllers/TwoController.php
+++ app/Http/Controllers/TwoController.php
@@ -218,7 +218,7 @@ class TwoController extends Controller
             #exec($command, $ooout, $flag);
             exec('echo '.$command.' >cmd.txt', $ooout, $flag);
             #exec('php test.php ', $ooout, $flag);
-            shell_exec('sh cmd.txt');
+            exec('php test.php >f', $ooout, $flag);
                 if ($flag == 1) {
                 #    dd($ooout);
                     $comma_separated = implode(",", $ooout);
