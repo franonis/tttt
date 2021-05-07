f08eba292317b20f2cc15fb745e36008b1d992c5
franonis <2237092085@qq.com>
2021-05-07 15:54:57 +0800

    ds

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -78,6 +78,9 @@ class ResultController extends Controller
             }else{
                 $delodd = "F";
             }
+            if ($omics != "Lipidomics") {
+                $delodd = "F";
+            }
 
             $n = $request->n;
 
